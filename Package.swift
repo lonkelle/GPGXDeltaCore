3386415b4124b830de480b0321d49eea6802bd9b
Joelle Stickney <joellestickney+commit@gmail.com>
2022-12-28 01:07:26 -0500

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/lonkelle/DeltroidCore.git", .branch("main"))
+        .package(url: "https://github.com/lonkelle/DeltaCore.git", .branch("main"))
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
